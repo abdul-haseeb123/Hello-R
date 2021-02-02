bd938cc4c5653c2a8ae240a383890f2b0de8a599
abdul-haseeb123 <syedabdulhaseeb611@gmail.com>
2021-02-02 10:50:12 +0500

    Few lines added to check the commit command,

--- testing.r
+++ testing.r
@@ -1,2 +1,9 @@
 # This is my first github file which extension is in R format
 print("Hello Data Science Community")
+
+name = "Syed Abdul Haseeb"
+
+print(name)
+print("This file is been edited to check the check in github repository url")
+print("The change has been completed......")
+print("Hurray")
